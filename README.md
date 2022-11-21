fe6967f3c010a6fc7b46d15a85fb5b945e2a6c43
Gayoung Kim <52883505+namiein@users.noreply.github.com>
2022-11-21 22:58:16 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# REACT TYPESCRIPT TEMPLATE
+
+## ABOUT
+
+React-TypeScript Template.  
+Demonstrating how I use REACT.js with TYPESCRIPT
+
+## TODO
+
+-   [ ] Change Folder Structure
+-   [ ] Add Folder Tree in README.md
+-   [ ] Create Sample Page
+
+## ACKNOWLEDGMENT
+
+### NPM Packages
+
+-   react v18
+-   react-query v4
+-   react-router-dom v6
+-   tailwindcss
+-   typescript
+
+### Code Convention
+
+-   ESLint - Airbnb
+-   ESLint + TypeScript
+-   Prettier
---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import ReactDOM from 'react-dom/client';
+
+import App from 'app/App';
+import 'styles/index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
+
+root.render(
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    </React.StrictMode>
+);

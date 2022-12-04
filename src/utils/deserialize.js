84876e65ca6e9f3f80db8534620d4549cb46f696
김가영 <namiein@naver.com>
2022-12-04 12:27:06 +0900

    feat : serialize, deserialize -> custom elements 추가

---
+++ src/utils/deserialize.js
@@ -0,0 +1,77 @@
+import { jsx } from 'slate-hyperscript';
+
+const deserialize = (el, markAttributes = {}) => {
+    if (el.nodeType === Node.TEXT_NODE) {
+        return jsx('text', markAttributes, el.textContent);
+    }
+    if (el.nodeType !== Node.ELEMENT_NODE) {
+        return null;
+    }
+
+    const nodeAttributes = { ...markAttributes };
+
+    // define attributes for text nodes
+    // Custom Formatting
+    switch (el.nodeName) {
+        case 'STRONG':
+            nodeAttributes.bold = true;
+            break;
+        case 'EM':
+            nodeAttributes.italic = true;
+            break;
+        case 'U':
+            nodeAttributes.underline = true;
+            break;
+        case 'DEL':
+            nodeAttributes.strikeThrough = true;
+            break;
+        default:
+            break;
+    }
+
+    const children = Array.from(el.childNodes)
+        .map((node) => deserialize(node, nodeAttributes))
+        .flat();
+
+    if (children.length === 0) {
+        children.push(jsx('text', nodeAttributes, ''));
+    }
+
+    // Custom Element
+    switch (el.nodeName) {
+        case 'BODY':
+            return jsx('fragment', {}, children);
+        case 'BR':
+            return '\n';
+        case 'H1':
+            return jsx('element', { type: 'heading-one' }, children);
+        case 'H2':
+            return jsx('element', { type: 'heading-two' }, children);
+        case 'H3':
+            return jsx('element', { type: 'heading-three' }, children);
+        case 'H4':
+            return jsx('element', { type: 'heading-four' }, children);
+        case 'BLOCKQUOTE':
+            return jsx('element', { type: 'quote' }, children);
+        case 'UL':
+            return jsx('element', { type: 'bulleted-list' }, children);
+        case 'OL':
+            return jsx('element', { type: 'numbered-list' }, children);
+        case 'LI':
+            return jsx('element', { type: 'list-item' }, children);
+        case 'CHECKLIST':
+            return jsx('element', { type: 'check-list' }, children);
+        case 'CHECKLISTITEM':
+            return jsx('element', { type: 'check-list-item' }, children);
+        case 'IMG':
+            return jsx('element', { type: 'image', url: el.getAttribute('src') }, children);
+        case 'A':
+            return jsx('element', { type: 'link', url: el.getAttribute('href') }, children);
+        case 'P':
+            return jsx('element', { type: 'paragraph' }, children);
+        default:
+            return children;
+    }
+};
+
+export default deserialize;
--- src/utils/serialize.js
+++ src/utils/serialize.js
@@ -1,8 +1,7 @@
 import escapeHTML from 'escape-html';
 import { Text } from 'slate';
-import { jsx } from 'slate-hyperscript';
 
-export const serialize = (node) => {
+const serialize = (node) => {
     if (Text.isText(node)) {
         // Custom Formatting
         let string = escapeHTML(node.text);
@@ -28,67 +27,35 @@ export const serialize = (node) => {
 
     // Custom Elements
     switch (node.type) {
-        case 'quote':
-            return `<blockquote><p>${children}</p></blockquote>`;
-        case 'paragraph':
-            return `<p>${children}</p>`;
+        case 'heading-one':
+            return `<h1>${children}</h1>`;
+        case 'heading-two':
+            return `<h2>${children}</h2>`;
+        case 'heading-three':
+            return `<h3>${children}</h3>`;
+        case 'heading-four':
+            return `<h4>${children}</h4>`;
+        case 'block-quote':
+            return `<blockquote>${children}</blockquote>`;
+        case 'numbered-list':
+            return `<ul className="orderedList>${children}</ol>`;
+        case 'bulleted-list':
+            return `<ul>${children}</ul>`;
+        case 'list-item':
+            return `<li>${children}</ul>`;
+        case 'check-list':
+            return `<checklist>${children}</checklist>`;
+        case 'check-list-item':
+            return `<checklistitem>${children}</checklistitem>`;
+        case 'image':
+            return `<img src="${escapeHTML(node.url)}" alt="custom img" />`;
         case 'link':
             return `<a href="${escapeHTML(node.url)}">${children}</a>`;
+        case 'paragraph':
+            return `<p>${children}</p>`;
         default:
             return children;
     }
 };
 
-export const deserialize = (el, markAttributes = {}) => {
-    if (el.nodeType === Node.TEXT_NODE) {
-        return jsx('text', markAttributes, el.textContent);
-    }
-    if (el.nodeType !== Node.ELEMENT_NODE) {
-        return null;
-    }
-
-    const nodeAttributes = { ...markAttributes };
-
-    // define attributes for text nodes
-    // Custom Formatting
-    switch (el.nodeName) {
-        case 'STRONG':
-            nodeAttributes.bold = true;
-            break;
-        case 'EM':
-            nodeAttributes.italic = true;
-            break;
-        case 'U':
-            nodeAttributes.underline = true;
-            break;
-        case 'DEL':
-            nodeAttributes.strikeThrough = true;
-            break;
-        default:
-            break;
-    }
-
-    const children = Array.from(el.childNodes)
-        .map((node) => deserialize(node, nodeAttributes))
-        .flat();
-
-    if (children.length === 0) {
-        children.push(jsx('text', nodeAttributes, ''));
-    }
-
-    // Custom Element
-    switch (el.nodeName) {
-        case 'BODY':
-            return jsx('fragment', {}, children);
-        case 'BR':
-            return '\n';
-        case 'BLOCKQUOTE':
-            return jsx('element', { type: 'quote' }, children);
-        case 'P':
-            return jsx('element', { type: 'paragraph' }, children);
-        case 'A':
-            return jsx('element', { type: 'link', url: el.getAttribute('href') }, children);
-        default:
-            return children;
-    }
-};
+export default serialize;
